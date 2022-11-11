59a01ba280f41a892332e741c1c6aa9833bd6500
Amal <amalscreations04@gmail.com>
2022-11-11 14:04:38 +0530

    http server started on port 3000

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("hello.wourld");
+import express, { Request, Response } from 'express';
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req: Request, res: Response) => {
+    console.log('hello wourld');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening localhost:${port}`);
+});
